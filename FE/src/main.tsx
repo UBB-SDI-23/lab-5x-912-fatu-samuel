67515abef7967f15c869de9d48fa17222b18a2e2
SamuelSamy <fatu.samuel24@gmail.com>
2023-04-01 11:49:00 +0300

    Update main.tsx

--- FE/src/main.tsx
+++ FE/src/main.tsx
@@ -4,7 +4,7 @@ import App from './App'
 import './index.css'
 
 // export const API_URL = 'http://46.101.118.44/main'
-export const API_URL = '/api'
+export const API_URL = 'api'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>

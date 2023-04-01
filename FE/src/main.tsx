293602f7739e91f3f0068eb5a4f068d3f902dc0b
SamuelSamy <fatu.samuel24@gmail.com>
2023-04-01 17:21:48 +0300

    fix

--- FE/src/main.tsx
+++ FE/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-export const API_URL = '/api'
+export const API_URL = 'api'
 // export const API_URL = 'http://46.101.118.44/main'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(

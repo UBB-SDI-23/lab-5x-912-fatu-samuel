6064b1370597c49149b0f981040eb3baa26f7f36
SamuelSamy <fatu.samuel24@gmail.com>
2023-04-24 11:11:35 +0300

    Better Pagination

--- uni-fe/src/components/ui-components/Pagination.tsx
+++ uni-fe/src/components/ui-components/Pagination.tsx
@@ -15,41 +15,73 @@ interface PaginatorProps {
 export const Paginator = ({ rowsPerPage, totalRows, currentPage, isFirstPage, isLastPage, setPage, goToNextPage, goToPrevPage }: PaginatorProps) => {
 
     const totalPages = Math.ceil(totalRows / rowsPerPage);
+    const hardPagesFirst = [1, 2]
+    const hardPagesLast = [totalPages - 1, totalPages]
+    const numberOfPages = 2;
 
-    const changeCurrentPage = (pageNumber: number) => {
-        if (pageNumber < 1) {
-            setPage(1);
+    let pages: number[] = [];
+    let dots: number = 0;
+
+    const getCurrentPages = () => {
+
+        for (let i = currentPage - numberOfPages; i <= currentPage + numberOfPages; i++) {
+            if (i > 0 && i <= totalPages) {
+                pages.push(i)
+            }
+        }
+
+        const commonFirst = pages.filter(element => hardPagesFirst.includes(element));
+        if (commonFirst.length != 0) {
+            pages = pages.filter(function (el) {
+                return !commonFirst.includes(el);
+            })
+
+            dots = 1;
             return;
         }
 
-        else if (pageNumber > totalPages) {
-            setPage(totalPages);
+        if (pages[0] == hardPagesFirst[numberOfPages - 1] + 1) {
+            dots = 1;
             return;
         }
 
-        setPage(pageNumber);
+        const commonLast = pages.filter(element => hardPagesLast.includes(element));
+        if (commonLast.length != 0) {
+            pages = pages.filter(function (el) {
+                return !commonLast.includes(el);
+            })
+
+            dots = -1;
+            return;
+        }
+
+        if (pages[pages.length - 1] == hardPagesLast[0] - 1) {
+            dots = -1;
+            return;
+        }
     }
 
-    const debounceOnChange = useCallback(debounce(changeCurrentPage, 500), []);
-
-    useEffect(() => {
-        return () => {
-            debounceOnChange.cancel();
-        };
-    }, [debounceOnChange])
+    getCurrentPages();
 
     return (
         <div className='pagination'>
 
-            <button className='floating' disabled={isFirstPage} onClick={() => setPage(1)}>First</button>
-            <button className='floating' disabled={isFirstPage} onClick={() => goToPrevPage()}>Prev</button>
+            <button className={currentPage === 1 ? 'active floating' : 'floating'} onClick={() => setPage(1)}>1</button>
+            <button className={currentPage === 2 ? 'active floating' : 'floating'} onClick={() => setPage(2)}>2</button>
 
-            <span className='floating'>
-                Page <input onChange={(event) => debounceOnChange(Number(event.target.value))} type="number" className='page-input' defaultValue={currentPage} /> of <strong>{totalPages}</strong>
-            </span>
+            {(dots == -1 || dots == 0) && <span className='floating'>...</span>}
 
-            <button className='floating' disabled={isLastPage} onClick={() => goToNextPage()}>Next</button>
-            <button className='floating' disabled={isLastPage} onClick={() => setPage(totalPages)}>Last</button>
+            {pages.map((page) => (
+                <button className={currentPage === page ? 'active floating' : 'floating'} onClick={() => setPage(page)}>{page}</button>
+            ))}
+
+
+            {(dots == 1 || dots == 0) && <span className='floating'>...</span>}
+
+
+            <button className={currentPage === totalPages - 1 ? 'active floating' : 'floating'} onClick={() => setPage(totalPages - 1)}>{totalPages - 1}</button>
+            <button className={currentPage === totalPages - 2 ? 'active floating' : 'floating'} onClick={() => setPage(totalPages)}>{totalPages}</button>
+
         </div >
     )
 }
